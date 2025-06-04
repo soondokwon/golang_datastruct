01d3e3d35f58b8d17a54e523fbb7c0483037c605
soondokwon <kwonsoondo@naver.com>
2025-06-04 22:01:27 +0900

    Add Rust implementation for linked list

---
+++ rust/src/linked_list.rs
@@ -0,0 +1,63 @@
+use std::collections::LinkedList;
+
+#[derive(Clone, Debug)]
+struct Node {
+    num: i32,
+    name: String,
+}
+
+pub struct DbLinkedList {
+    list: LinkedList<Node>,
+}
+
+impl DbLinkedList {
+    pub fn new() -> Self {
+        DbLinkedList {
+            list: LinkedList::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, num: i32, name: &str) {
+        self.list.push_back(Node { num, name: name.to_string() });
+    }
+
+    pub fn remove_node(&mut self, num: i32) {
+        let mut new_list = LinkedList::new();
+        while let Some(node) = self.list.pop_front() {
+            if node.num != num {
+                new_list.push_back(node);
+            }
+        }
+        self.list = new_list;
+    }
+
+    pub fn print(&self) {
+        for node in self.list.iter() {
+            println!("num=[{}], name=[{}]", node.num, node.name);
+        }
+    }
+
+    pub fn print_reverse(&self) {
+        for node in self.list.iter().rev() {
+            println!("num=[{}], name=[{}]", node.num, node.name);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_ops() {
+        let mut list = DbLinkedList::new();
+        list.add_node(1, "benny");
+        list.add_node(2, "john");
+        list.add_node(3, "tommy");
+        assert_eq!(list.list.len(), 3);
+        list.remove_node(2);
+        assert_eq!(list.list.len(), 2);
+        let names: Vec<_> = list.list.iter().map(|n| n.name.clone()).collect();
+        assert_eq!(names, vec!["benny".to_string(), "tommy".to_string()]);
+    }
+}
